faaaa8a3e05bba34a7b8aa90d94a7254f8325333
Grzegorz Mandziak <alimek@alimek.pl>
2019-02-11 21:49:25 +0100

    Updates

---
+++ precache-manifest.3712ab4ac4a753a7cdcd3fd4baf71e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "4a440546d71029eb659a",
+    "url": "/portfolio/static/js/main.4a440546.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/portfolio/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "4a440546d71029eb659a",
+    "url": "/portfolio/static/css/main.4358bda9.chunk.css"
+  },
+  {
+    "revision": "ea638d7ba4eb807eec59626975696e5e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0174b0e513bac8879ddb09b38a6b0cd0.js"
+  "/portfolio/precache-manifest.3712ab4ac4a753a7cdcd3fd4baf71e5d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
