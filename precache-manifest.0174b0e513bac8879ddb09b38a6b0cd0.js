a5fce172b1bfb530e7e16db19dbc032a1b1d9760
Grzegorz Mandziak <alimek@alimek.pl>
2019-02-11 21:07:22 +0100

    Updates

---
+++ precache-manifest.0174b0e513bac8879ddb09b38a6b0cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4cafa87999999db251a3",
+    "url": "/static/js/main.4cafa879.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "4cafa87999999db251a3",
+    "url": "/static/css/main.4358bda9.chunk.css"
+  },
+  {
+    "revision": "4df0a9358dc5bb9da42a28efca42a694",
+    "url": "/index.html"
+  }
+];
